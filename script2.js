516b410995b7aae43e0d5c91c750fab33060dc22
ezhilk14 <108933770+ezhilk14@users.noreply.github.com>
2022-10-12 12:24:54 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+function foo(){
+    var res1=document.getElementById("fname").value;
+    var res2=document.getElementById("mname").value;
+    var res3=document.getElementById("lname").value;
+var res4=document.getElementById("email").value;
+var res5=document.getElementById("pass").value;
+console.log(res1,res2,res3,res4,res5);
+}
